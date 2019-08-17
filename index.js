be6b0af7dacf8300ec84a0b6bb536e345ddfc9c1
Fernando Cristobal De La Cruz <fernandocgomez@live.com>
2019-08-17 04:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.") {
+  else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
